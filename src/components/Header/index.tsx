0b496533ba789ffe8c83ad4b5c66a9ebf42f1c69
Jean Lincoln <jeanlincol@gmail.com>
2022-08-13 08:18:20 -0700

    Header component, radix dialog structure added, react portal used

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,27 @@
-import Logo from '../../assets/Logo.svg'
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
+import Logo from '../../assets/Logo.svg'
+
 
 export function Header(){
     return(
         <HeaderContainer>
             <HeaderContent>
                 <img src={Logo} alt="" />
-                <NewTransactionButton>
-                    Make a transaction
-                </ NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>
+                            Make a transaction
+                        </ NewTransactionButton>
+                    </Dialog.Trigger>
+                    <Dialog.Portal>
+                        <Dialog.Overlay />
+                        <Dialog.Content>
+                            <Dialog.Title>Nova Transação</Dialog.Title>
+                            <Dialog.Close />
+                        </Dialog.Content>
+                    </Dialog.Portal>
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     )
