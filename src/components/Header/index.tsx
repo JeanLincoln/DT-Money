e26dbc56fb978c38c1a014c5e2b6c0039c16bc4c
Jean Lincoln <jeanlincol@gmail.com>
2022-08-13 09:07:32 -0700

    NewTransactionModal Created, styles and structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 import * as Dialog from '@radix-ui/react-dialog'
 import Logo from '../../assets/Logo.svg'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 
 export function Header(){
@@ -14,13 +15,7 @@ export function Header(){
                             Make a transaction
                         </ NewTransactionButton>
                     </Dialog.Trigger>
-                    <Dialog.Portal>
-                        <Dialog.Overlay />
-                        <Dialog.Content>
-                            <Dialog.Title>Nova Transação</Dialog.Title>
-                            <Dialog.Close />
-                        </Dialog.Content>
-                    </Dialog.Portal>
+                    <NewTransactionModal/>
                 </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
